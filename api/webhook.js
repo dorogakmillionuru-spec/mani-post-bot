3d5a62aec3cde2399221b44870c5f60f82ebe0fe
dorogakmillionuru-spec <dorogakmillionu.ru@gmail.com>
2026-01-24 21:43:33 +0300

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,15 +1,3 @@
-export default async function handler(req, res) {
-  if (req.method !== "POST") {
-    return res.status(200).send("ok");
-  }
-
-  const body = req.body || {};
-  const chatId = body?.message?.chat?.id;
-  const userText = body?.message?.text || "";
-
-  if (!chatId) {
-    return res.status(200).send("no chat");
-  }
-
-  return res.status(200).send("Толик на связи");
+export default function handler(req, res) {
+  return res.status(200).send("alive");
 }
